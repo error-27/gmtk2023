98d0f5b34d94329bab176d58966f8fd88ad0ccc9
[Error_27] <error27dev@gmail.com>
2023-07-08 13:06:40 -0400

    Connect level to game

--- scripts/ui/CharacterSelect.gd
+++ scripts/ui/CharacterSelect.gd
@@ -1,4 +1,9 @@
 extends Control
 
 func _selected():
-	print("Should change to first level")
+	var intro_res = preload("res://scenes/level/LevelIntro.tscn")
+	var intro = intro_res.instance()
+	intro.level_num = 1
+	intro.next_lvl = "res://scenes/level/Demo1.tscn"
+	get_viewport().add_child(intro)
+	queue_free()
