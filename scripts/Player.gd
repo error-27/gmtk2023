20283854f709504c5385b293f93d046d8f0e45de
[Error_27] <error27dev@gmail.com>
2023-07-08 19:52:14 -0400

    Add player animations

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,27 +7,37 @@ export(int) var move_speed = 150
 export(int) var floor_friction = 20
 export(int) var jump_in_air = -45
 
+onready var animator = $AnimationPlayer
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
 	apply_gravity()
 	var input = Vector2.ZERO
 	input.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	if input.x > 0:
+		$Sprite.flip_h = false
+	elif input.x < 0:
+		$Sprite.flip_h = true
 	
 	if input.x == 0:
 		apply_friction()
+		animator.stop()
+		if is_on_floor(): $Sprite.frame = 0
 	else:
 		apply_acceleration(input.x)
+		if is_on_floor(): animator.play("run")
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("Jump"):
 			velocity.y = jump_height
+			animator.stop()
 	else:
 		if Input.is_action_just_released("Jump") and velocity.y < jump_in_air:
 			velocity.y = jump_in_air
 			
 		if velocity.y > 6:
 			velocity.y += 4
+		$Sprite.frame = 5
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
