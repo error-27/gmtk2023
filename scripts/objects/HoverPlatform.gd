35043116d368c05886b57676001b7b0125fce0e0
[Error_27] <error27dev@gmail.com>
2023-07-08 08:50:59 -0400

    Vertical reversal and reset position

--- scripts/objects/HoverPlatform.gd
+++ scripts/objects/HoverPlatform.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 export var horizontal: bool = true
 export var vertical: bool = false
 export var reverse_horizontal: bool = false
+export var reverse_vertical: bool = false
 
 var dir: Vector2 = Vector2.ZERO
 
@@ -17,12 +18,16 @@ func _ready():
 	$Panel/VBoxContainer/Horizontal.pressed = horizontal
 	$Panel/VBoxContainer/Vertical.pressed = vertical
 	$Panel/VBoxContainer/Reverse.pressed = reverse_horizontal
+	$Panel/VBoxContainer/Reverse2.pressed = reverse_vertical
 	for child in $Panel/VBoxContainer.get_children():
 		child.connect("toggled", self, "_update_configs", [child.name])
 
 func _process(delta):
-	var reversal = 1 if not reverse_horizontal else -1
-	dir = Vector2( reversal * int(horizontal) * timer.time_left / timer.wait_time, int(vertical) * timer.time_left / timer.wait_time)
+	if $Panel.visible:
+		return
+	var reversal_h = 1 if not reverse_horizontal else -1
+	var reversal_v = 1 if not reverse_vertical else -1
+	dir = Vector2(reversal_h * int(horizontal) * timer.time_left / timer.wait_time, reversal_v * int(vertical) * timer.time_left / timer.wait_time)
 	move_and_collide(dir * speed)
 
 
@@ -36,6 +41,8 @@ func _update_configs(pressed: bool, property: String):
 		vertical = pressed
 	elif property == "Reverse":
 		reverse_horizontal = pressed
+	elif property == "Reverse2":
+		reverse_vertical = pressed
 	
 	position = start_pos
 	timer.start(timer.wait_time)
@@ -45,3 +52,6 @@ func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_RIGHT && event.pressed:
 			$Panel.visible = not $Panel.visible
+			position = start_pos
+			if $Panel.visible:
+				timer.start(timer.wait_time)
