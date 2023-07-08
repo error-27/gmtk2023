26f5c1f47ff2e5c70aa28f5ce4d7ab97ef1c5db3
[Error_27] <error27dev@gmail.com>
2023-07-07 23:54:01 -0400

    Add config menu

--- scripts/objects/HoverPlatform.gd
+++ scripts/objects/HoverPlatform.gd
@@ -10,9 +10,15 @@ onready var timer = $Timer
 
 export var speed: float = 1
 
+onready var start_pos = position
+
 func _ready():
 	$AnimationPlayer.play("gear_spin")
-	
+	$Panel/VBoxContainer/Horizontal.pressed = horizontal
+	$Panel/VBoxContainer/Vertical.pressed = vertical
+	$Panel/VBoxContainer/Reverse.pressed = reverse_horizontal
+	for child in $Panel/VBoxContainer.get_children():
+		child.connect("toggled", self, "_update_configs", [child.name])
 
 func _process(delta):
 	var reversal = 1 if not reverse_horizontal else -1
@@ -22,3 +28,20 @@ func _process(delta):
 
 func _on_Timer_timeout():
 	speed *= -1
+
+func _update_configs(pressed: bool, property: String):
+	if property == "Horizontal":
+		horizontal = pressed
+	elif property == "Vertical":
+		vertical = pressed
+	elif property == "Reverse":
+		reverse_horizontal = pressed
+	
+	position = start_pos
+	timer.start(timer.wait_time)
+
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_RIGHT && event.pressed:
+			$Panel.visible = not $Panel.visible
