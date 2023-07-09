43d65b95bf60bc36a79f26e0abb07f063462ac21
[Error_27] <error27dev@gmail.com>
2023-07-09 00:08:48 -0400

    Add level finishes and fix edit gui

--- scripts/level/Level.gd
+++ scripts/level/Level.gd
@@ -1,5 +1,20 @@
 extends Node2D
 
+export var finish_line: NodePath
+onready var finish = get_node(finish_line)
+
+export var next_lvl_num: int = 1
+export(String, FILE, "*.tscn,*.scn") var next_lvl
+export var player: NodePath
 
 func _ready():
-	pass
+	(finish as Area2D).connect("body_entered", self, "transition_next_lvl")
+
+func transition_next_lvl(body):
+	if body == get_node(player):
+		var intro_res = preload("res://scenes/level/LevelIntro.tscn")
+		var intro = intro_res.instance()
+		intro.level_num = next_lvl_num
+		intro.next_lvl = next_lvl
+		get_viewport().add_child(intro)
+		queue_free()
