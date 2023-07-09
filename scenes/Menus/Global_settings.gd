bbf68a7a94d8d3082de45a95abea9c4b9174514c
Wired-Weasel <138394392+Wired-Weasel@users.noreply.github.com>
2023-07-09 06:56:36 -0400

    Added onto demo level

---
+++ scenes/Menus/Global_settings.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func toggle_fullscreen(value):
+	OS.window_fullscreen(value)
+
+func toggle_vsync(value):
+	OS.vsync_enabled(value)
+
+
+
--- scenes/Menus/pausescreen.gd
+++ scenes/Menus/pausescreen.gd
@@ -5,16 +5,6 @@ extends Panel
 
 func _ready():
 	pass
-	#var pause_enable = false
-	#if Input.action_press("Pause"):
-	
-	#	pause_enable = true
-	#while pause_enable == true:
-	#	if Input.action_press("Pause"):
-	#		visible = false
-	#		pause_enable = false
-	#	else:
-	#		pass
 
 
 onready var fullscreen_setting = $"Settings tabs/General/MarginContainer/GridContainer/Fullscreen/fullcheck"
@@ -25,15 +15,17 @@ onready var displayfps_setting = $"Settings tabs/Graphics/MarginContainer/GridCo
 
 
 func _on_fullcheck_toggled(button_pressed):
-	pass # Replace with function body.
+	pass
 
 
-func _on_vscheck_toggled(button_pressed):
-	pass # Replace with function body.
+
+func _on_vscheck_toggled(button_pressed):  # set up global settings script so this can persist
+	pass
+
 
 
 func _on_potatoslider_value_changed(value):
-	pass # Replace with function body.
+	pass # This is intentional, It's Pixel art
 
 
 func _on_maxfps_button_item_selected(index):
@@ -41,4 +33,4 @@ func _on_maxfps_button_item_selected(index):
 
 
 func _on_FPS_button_toggled(button_pressed):
-	pass # Replace with function body.
+	pass
